f0c18a66d869ed78972989f42bdcf6c00e4a22fb
Wanjala Stephen <stephenwanjala145@gmail.com>
2024-08-13 16:28:06 +0300

    edit Docusaurus Version

--- my-docs/docusaurus.config.ts
+++ my-docs/docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/stephenWanjala/kenyanCounties/edit/master/my-docs/docs/api-overview.md',
+            'https://github.com/stephenWanjala/kenyanCounties/edit/master/my-docs/',
         },
         // blog: {
         //   showReadingTime: true,
